5ea7ed7b37bc9bae34435bb209ff886b610df661
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-06-15 00:58:34 +0300

    EntityConfigurations are added.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/EntityConfigurations/BaseEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSozluk.Infrastructure.Persistence.EntityConfigurations;
+
+public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<T> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Id).ValueGeneratedOnAdd();
+        builder.Property(i => i.CreateDate).ValueGeneratedOnAdd();
+    }
+}
