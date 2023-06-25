eeb20b8128bb1b5f72aecd1502baceb03d582bd2
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-06-25 15:58:15 +0300

    Login Handle works

---
+++ src/Common/BlazorSozluk.Common/Models/RequestModels/LoginUserCommand.cs
@@ -0,0 +1,21 @@
+﻿using BlazorSozluk.Common.Models.Queries;
+using MediatR;
+
+namespace BlazorSozluk.Common.ViewModels.RequestModels;
+
+public class LoginUserCommand : IRequest<LoginUserViewModel>
+{
+    public string EmailAddress { get; private set; }
+    public string Password { get; private set; }
+
+    public LoginUserCommand(string emailAddress, string password)
+    {
+        EmailAddress = emailAddress;
+        Password = password;
+    }
+
+    public LoginUserCommand()
+    {
+
+    }
+}
