cdb764608ef87c08d54d028f241efafabb565d00
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-07-12 00:15:31 +0300

    UserCommands and handlers

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/Create/CreateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Common.Infrastructure.Exceptions;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User.Create;
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
+{
+    private readonly IMapper mapper;
+    private readonly IUserRepository userRepository;
+
+    public CreateUserCommandHandler(IMapper mapper, IUserRepository userRepository)
+    {
+        this.mapper = mapper;
+        this.userRepository = userRepository;
+    }
+
+    public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var existsUser = await userRepository.GetSingleAsync(i => i.EmailAddress == request.EmailAddress);
+
+        if (existsUser is not null)
+            throw new DatabaseValidationException("User already exists!");
+
+        var dbUser = mapper.Map<Domain.Models.User>(request);
+
+        var rows = await userRepository.AddAsync(dbUser);
+    }
+}
