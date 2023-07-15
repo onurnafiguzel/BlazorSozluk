7650e85aca472d96d6df38e27fa522996511f7ef
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-07-16 02:01:10 +0300

    SozlukConstants extended

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/Entry/CreateFav/CreateEntryCommentFavCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using BlazorSozluk.Common;
+using BlazorSozluk.Common.Infrastructure;
+using MediatR;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.Entry.CreateFav;
+
+public class CreateEntryCommentFavCommandHandler : IRequestHandler<CreateEntryCommentFavCommand, bool>
+{
+    public Task<bool> Handle(CreateEntryCommentFavCommand request, CancellationToken cancellationToken)
+    {
+        QueueFactory.SendMessageToExchange(exchangeName: SozlukConstants.FavExchangeName)
+    }
+}
