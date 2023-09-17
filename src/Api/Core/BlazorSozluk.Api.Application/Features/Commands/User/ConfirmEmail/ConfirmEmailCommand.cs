6cf4835ea75bdbd5eacd0fafc4ab41cf9b1b9618
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-09-17 21:38:51 +0300

    ConfirmEmail Comamnd and handler and update mappingprofile

---
+++ src/Api/Core/BlazorSozluk.Api.Application/Features/Commands/User/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BlazorSozluk.Api.Application.Features.Commands.User.ConfirmEmail;
+
+public class ConfirmEmailCommand : IRequest<bool>
+{
+    public Guid ConfirmationId { get; set; }
+}
