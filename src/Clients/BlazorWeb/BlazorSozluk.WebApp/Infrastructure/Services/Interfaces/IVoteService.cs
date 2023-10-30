fe33e761d1f0de3051d2db09228f36490f391b3e
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-10-30 23:02:08 +0300

    UI Services, backend for frontend

---
+++ src/Clients/BlazorWeb/BlazorSozluk.WebApp/Infrastructure/Services/Interfaces/IVoteService.cs
@@ -0,0 +1,9 @@
+﻿public interface IVoteService
+{
+    Task CreateEntryCommentDownVote(Guid entryCommentId);
+    Task CreateEntryCommentUpVote(Guid entryCommentId);
+    Task CreateEntryDownVote(Guid entryCommentId);
+    Task CreateEntryUpVote(Guid entryId);
+    Task DeleteEntryCommentVote(Guid entryCommentId);
+    Task DeleteEntryVote(Guid entryId);
+}
--- src/Clients/BlazorWeb/BlazorSozluk.WebApp/Program.cs
+++ src/Clients/BlazorWeb/BlazorSozluk.WebApp/Program.cs
@@ -1,11 +1,38 @@
 using BlazorSozluk.WebApp;
+using BlazorSozluk.WebApp.Infrastructure.Services.Interfaces;
+using BlazorSozluk.WebApp.Infrastructure.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = builder.Configuration["BaseUrl"];
+builder.Services.AddHttpClient("WebApiClient", client =>
+{
+    client.BaseAddress = new Uri(baseUrl);
+    //TODO AuthTokenHandler will be here
+});
+
+builder.Services.AddScoped(sp =>
+{
+    var clientFactory = sp.GetRequiredService<IHttpClientFactory>();
+
+    return clientFactory.CreateClient("WebApiClient");
+});
+
+#region Services
+
+builder.Services.AddTransient<IEntryService, EntryService>();
+builder.Services.AddTransient<IVoteService, VoteService>();
+builder.Services.AddTransient<IFavService, FavService>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IIdentityService, IdentityService>();
+
+#endregion
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
