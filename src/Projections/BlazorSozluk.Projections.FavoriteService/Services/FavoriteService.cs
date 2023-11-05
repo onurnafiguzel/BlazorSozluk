97963c0a51fb3be2bad0985ccc85ef2d9650e5c6
Onur Nafi Guzel <nafi.guzel@consultant.boyner.com.tr>
2023-11-05 22:06:13 +0300

    Projections VoteService and FavService's worker clas are done for RabbitMq exchange

---
+++ src/Projections/BlazorSozluk.Projections.FavoriteService/Services/FavoriteService.cs
@@ -0,0 +1,30 @@
+﻿using BlazorSozluk.Common.Events.Entry;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BlazorSozluk.Projections.FavoriteService.Services;
+
+public class FavoriteService
+{
+    private readonly string connectionString;
+
+    public FavoriteService(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public async Task CreateEntryFav(CreateEntryFavEvent @event)
+    {
+        using var connection = new SqlConnection();
+
+        await connection
+           .ExecuteAsync("INSERT INTO EntryFavorite (Id, EntryId, CreatedById, CreateDate) VALUES(@Id, @EntryId, @CreatedById, GETDATE())",
+           new
+           {
+               Id = Guid.NewGuid(),
+               EntryId = @event.EntryId,
+               CreatedById = @event.CreatedBy
+           });
+
+    }
+}
